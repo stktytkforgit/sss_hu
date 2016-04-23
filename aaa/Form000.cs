cecb120ea63d786038695aa4251f9ed8a930e99d
stktytk <stktytkforgit@gmail.com>
2016-04-23 19:20:46 +0900

    First commit.
    This is first commit.

---
+++ aaa/Form000.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aaa
+{
+	public partial class Form000 : Form
+	{
+		public Form000()
+		{
+			InitializeComponent();
+		}
+	}
+}
